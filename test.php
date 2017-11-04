0f7b55cc6a57db3b1349693ccf6e73ba487c7cbd
yaahoomail <33373592+yaahoomail@users.noreply.github.com>
2017-11-04 19:16:21 +0530

    Update test.php

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
 
 <script>alert(document.domain)</script>
 
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS-3VU1zUBuBwxdmJOXj1TF5GieGLbLLs6cN9Chl4WBmsihtO9hg"></img
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS-3VU1zUBuBwxdmJOXj1TF5GieGLbLLs6cN9Chl4WBmsihtO9hg"></img>
 ?>
 
 </body>

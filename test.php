586073948e68ce8d299fdfbf5e62cd6640f0b6a3
yaahoomail <33373592+yaahoomail@users.noreply.github.com>
2017-11-04 19:14:40 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,16 @@
+
+  
+<html>
+
+<body>
+<?php
+<p> U have been hacked!!</p>
+
+<script>alert(document.domain)</script>
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS-3VU1zUBuBwxdmJOXj1TF5GieGLbLLs6cN9Chl4WBmsihtO9hg"></img
+?>
+
+</body>
+</html>
+
---
+++ test1.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+<?php
+<p>This is my Test Page</p>
+<script>alert("Xssed_By_TrapP3r_Hat")
+window.location="data:text/html;base64,PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pPC9zY3JpcHQ+"
+</script>
+?>
+
+</body>
+</html>
+
